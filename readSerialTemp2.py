90101c962b4f516c24a07b8e5dbaa0dcf1897077
hani-shawa <hani.shawa@ieee.org>
2013-10-21 23:16:12 +0400

    third

--- readSerialTemp2.py
+++ readSerialTemp2.py
@@ -10,7 +10,7 @@ from ConfigParser import SafeConfigParser
 
 
 
-# read configuration file in (current directory) config.ini
+# read configuration file in (current directory) simple.ini
 parser 		= SafeConfigParser()
 parser.read('simple.ini')
 
@@ -25,20 +25,21 @@ host 		= parser.get('database', 'host')
 port 		= int(parser.get('database', 'port'))
 user 		= parser.get('database', 'user')
 password 	= parser.get('database','password')
-
+database_name	= parser.get('database','database_name')
 
 
 # create database connection
 db = MySQLdb.connect(host=host, port=port, user=user, db="home_temperatures")
+
 cursor = db.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS home_temperatures(TimeStamp TIMESTAMP NOT NULL PRIMARY KEY, Temperature INT)");
+cursor.execute("CREATE TABLE IF NOT EXISTS %s(TimeStamp TIMESTAMP NOT NULL PRIMARY KEY, Temperature INT)",database_name);
 
 
 
-# open serial connection with first argument as device name (argVector[1]), baudrate 9600, and timeout 1s
+# open serial connection with device, baudrate and timeout 1s
 ser = serial.Serial(device,baudrate, timeout = 1)
 
-
+# query string
 query = "INSERT INTO temperatures(Temperature) VALUES(%s)"
 
 
@@ -47,10 +48,12 @@ notFound = True;
 # total size of reading from the arduino is 61 bytes 
 while (notFound) :
  s = ser.read(61)
-  # if 'C: ' is found in the buffer, print the 3-7 characters after it (whic is the temperature)
+  # if 'C: ' is found in the buffer, save the [3-7] characters after it (which is the temperature)
  if (s.find('C: ') >= 0):
-  #notFound = False;
+  
   currTemp = s[s.find('C: ') + 3:s.find('C: ')+7]
+  
+  # store in db
   cursor.execute(query,currTemp)
   
   db.commit
@@ -59,8 +62,3 @@ while (notFound) :
  
  # a little delay
  time.sleep(10);
- 
- 
- # what is left to do is outside the loop connect to the database, and inside the loop store the value
-
-
